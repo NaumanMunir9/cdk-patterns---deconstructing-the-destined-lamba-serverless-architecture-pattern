32f48cae662edc985c3860846f703c525a7160a0
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-01 09:17:12 +0500

    Initial commit

---
+++ lib/1-lambda-destination-stack.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class 1LambdaDestinationStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, '1LambdaDestinationQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
