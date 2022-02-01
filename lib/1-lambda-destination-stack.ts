6d925086c761f93270d5b1385919bfd316543d11
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-01 10:26:27 +0500

    apiGateway root addResource. This is the root resource of the API Gateway

--- lib/1-lambda-destination-stack.ts
+++ lib/1-lambda-destination-stack.ts
@@ -194,5 +194,98 @@ export class LambdaDestinationStack extends Stack {
         },
       }
     );
+
+    // =========================================================================
+    // apiGateway root addResource. This is the root resource of the API Gateway
+    // =========================================================================
+    apiGatewayRestApi.root.addResource("SendEvent").addMethod(
+      "GET",
+      new apigateway.Integration({
+        type: apigateway.IntegrationType.AWS, //native aws integration
+        integrationHttpMethod: "POST",
+        uri: "arn:aws:apigateway:us-east-1:sns:path//", // This is how we setup an SNS Topic publish operation.
+        options: {
+          credentialsRole: apiGatewayIamRole,
+          requestParameters: {
+            "integration.request.header.Content-Type":
+              "'application/x-www-form-urlencoded'", // Tell api gw to send our payload as query params
+          },
+          requestTemplates: {
+            // This is the VTL to transform our incoming request to post to our SNS topic
+            // Check: https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+            "application/json":
+              "Action=Publish&" +
+              "TargetArn=$util.urlEncode('" +
+              snsTopic.topicArn +
+              "')&" +
+              "Message=please $input.params().querystring.get('mode')&" +
+              "Version=2010-03-31",
+          },
+          passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+          integrationResponses: [
+            {
+              // Tells APIGW which response to use based on the returned code from the service
+              statusCode: "200",
+              responseTemplates: {
+                // Just respond with a generic message
+                // Check https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+                "application/json": JSON.stringify({
+                  message: "Message added to SNS topic",
+                }),
+              },
+            },
+            {
+              // For errors, we check if the response contains the words BadRequest
+              selectionPattern: "^[Error].*",
+              statusCode: "400",
+              responseTemplates: {
+                "application/json": JSON.stringify({
+                  state: "error",
+                  message:
+                    "$util.escapeJavaScript($input.path('$.errorMessage'))",
+                }),
+              },
+              responseParameters: {
+                "method.response.header.Content-Type": "'application/json'",
+                "method.response.header.Access-Control-Allow-Origin": "'*'",
+                "method.response.header.Access-Control-Allow-Credentials":
+                  "'true'",
+              },
+            },
+          ],
+        },
+      }),
+      {
+        methodResponses: [
+          //We need to define what models are allowed on our method response
+          {
+            // Successful response from the integration
+            statusCode: "200",
+            // Define what parameters are allowed or not
+            responseParameters: {
+              "method.response.header.Content-Type": true,
+              "method.response.header.Access-Control-Allow-Origin": true,
+              "method.response.header.Access-Control-Allow-Credentials": true,
+            },
+            // Validate the schema on the response
+            responseModels: {
+              "application/json": responseModel,
+            },
+          },
+          {
+            // Same thing for the error responses
+            statusCode: "400",
+            responseParameters: {
+              "method.response.header.Content-Type": true,
+              "method.response.header.Access-Control-Allow-Origin": true,
+              "method.response.header.Access-Control-Allow-Credentials": true,
+            },
+            responseModels: {
+              "application/json": errorResponseModel,
+            },
+          },
+        ],
+      }
+    );
   }
 }
